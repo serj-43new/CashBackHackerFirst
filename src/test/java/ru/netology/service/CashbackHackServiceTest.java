4345ff00c36d12886c68b1d3dae169a110a7e797
serj-43new <platunovaa@inbox.ru>
2022-09-06 22:07:56 +0300

    test added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCorrectAmount() {
+        assertEquals(service.remain(2400), 600);
+
+    }
+
+    @Test
+    public void shouldAmountZero() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldBuyExtraIfTheAmount1000() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+}
