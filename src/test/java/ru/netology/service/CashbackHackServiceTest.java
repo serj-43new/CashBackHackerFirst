8a42cc9191f2dbd7b9ecef3d802d18183fee819c
serj-43new <platunovaa@inbox.ru>
2022-09-07 07:19:48 +0300

    junit4 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
